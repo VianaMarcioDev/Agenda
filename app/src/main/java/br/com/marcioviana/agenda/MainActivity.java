c2f9bb63e7295f5da13599fe6a11b3ce4fdc5ff1
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-01-11 22:23:04 -0300

    Implementano método onCreate na MainActivity

--- app/src/main/java/br/com/marcioviana/agenda/MainActivity.java
+++ app/src/main/java/br/com/marcioviana/agenda/MainActivity.java
@@ -1,6 +1,15 @@
 package br.com.marcioviana.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Márcio Viana", Toast.LENGTH_LONG).show();
+    }
 }
